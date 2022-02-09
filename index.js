f262a89701462cfb13eeda7a7c88d3a9f90825d4
ArtemZhurnevich <zhur2305@gmail.com>
2022-02-09 15:44:06 +0300

    complete 30%

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var a;
 a = 7;
 // Only change code below this line
 
+
 // Setup
 var a;
 a = 7;
@@ -10,14 +11,80 @@ var b;
 b = a;
 // Only change code below this liney7
 
+
 // Only change code below this line
 var a = 5;
 var b = 10;
 var c = 'I am a';
 // Only change code above this line
-
 a = a + 1;
 b = b + 5;
 c = c + " String!";
+
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+
+let a = 11;
+let b = 9;
+let c = 3;
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+let a = 48;
+let b = 108;
+let c = 33;
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+// Change this line
+
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
+const myStr = "This is the start." + " This is the end."; // Change this line
+
+
+let myStr = "This is the first sentence.";
+myStr += " This is the second sentence.";
+
+
+// Only change code below this line
+const myName = "Artem";
+const myStr = "My name is" + myName + "and I am well!";
+
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+
+// Setup
+let myStr = "Jello World";
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
 
 
