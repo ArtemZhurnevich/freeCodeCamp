170bd69c47caee726633411113095737ddd5a594
ArtemZhurnevich <zhur2305@gmail.com>
2022-02-10 11:49:12 +0300

    completed 60%

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ b /= 4;
 c /= 11;
 
 
-const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 // Change this line
 
 
@@ -86,5 +86,270 @@ let myStr = "Jello World";
 // Only change code below this line
 myStr = "Hello World"; // Change this line
 // Only change code above this line
+
+
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+
+// Setup
+const lastName = "Lovelace";
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+
+// Only change code below this line
+const myArray = ["peanut butter", 4];
+
+
+// Only change code below this line
+const myArray = [["Bulls", 23], ["White Sox", 45]];
+
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+// Only change code below this line
+
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+let removedFromMyArray = myArray.pop();
+// Only change code below this line
+
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+const removedFromMyArray = myArray.shift();
+// Only change code below this line
+
+
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
+// Only change code below this line
+
+
+const myList = [
+    ["Chocolate Bar", 15],
+    ["Chocolate Bar", 15],
+    ["Chocolate Bar", 15],
+    ["Chocolate Bar", 15],
+    ["Chocolate Bar", 15],
+];
+
+
+function reusableFunction() {
+    console.log("Hi World");
+}
+reusableFunction();
+
+
+function functionWithArgs(param1, param2) {
+    console.log(param1 + param2);
+}
+functionWithArgs(1, 2);
+
+
+function timesFive(num) {
+    return num * 5;
+}
+const answer = timesFive(5);
+
+
+function myLocalScope() {
+    // Only change code below this line
+    let myVar = 14;
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);
+
+
+  // Setup
+const outerWear = "T-Shirt";
+function myOutfit() {
+  // Only change code below this line
+  let outerWear = "sweater"
+  // Only change code above this line
+  return outerWear;
+}
+myOutfit();
+
+
+// Setup
+let sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+// Only change code above this line
+addThree();
+addFive();
+
+
+// Setup
+let processed = 0;
+function processArg(num) {
+  return (num + 3) / 5;
+}
+processed = processArg(7);
+// Only change code below this line
+
+
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    item = arr.shift();
+    return item;
+    // Only change code above this line
+  }
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+
+
+  function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+  if (wasThatTrue) {
+      return "Yes, that was true";
+    }
+    return "No, that was false";
+    // Only change code above this line
+  }
+
+
+  // Setup
+function testEqual(val) {
+    if (val == 12) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  testEqual(10);
+
+
+// Setup
+function testStrict(val) {
+    if (val===7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  testStrict(10);
+
+
+// Setup
+function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  compareEquality(10, "10");
+
+
+// Setup
+function testNotEqual(val) {
+    if (val != 99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  testNotEqual(10);
+
+
+// Setup
+function testStrictNotEqual(val) {
+    if (val!==17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  testStrictNotEqual(10);
+
+
+  function testGreaterThan(val) {
+    if (val>100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val>10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  testGreaterThan(10);
+
+
+  function testGreaterOrEqual(val) {
+    if (val >= 20) {  // Change this line
+      return "20 or Over";
+    }
+  
+    if (val >=10) {  // Change this line
+      return "10 or Over";
+    }
+      return "Less than 10";
+  }
+  testGreaterOrEqual(10);
+
+
+  function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+    return "55 or Over";
+  }
+  testLessThan(10);
+
+
+  function testLessOrEqual(val) {
+    if (val<=12) {  // Change this line
+      return "Smaller Than or Equal to 12";
+    }
+  
+    if (val<=24) {  // Change this line
+      return "Smaller Than or Equal to 24";
+    }
+  
+    return "More Than 24";
+  }
+  testLessOrEqual(10);
+
+
+  
+
+
+
+
+
+
+
+
 
 
