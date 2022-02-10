35bc685f9e0fa9d7c75175c9360455b70024d909
ArtemZhurnevich <zhur2305@gmail.com>
2022-02-10 20:28:25 +0300

    completed 76%

--- index.js
+++ index.js
@@ -342,7 +342,321 @@ function testStrictNotEqual(val) {
   testLessOrEqual(10);
 
 
+function testLogicalAnd(val) {
+    // Only change code below this line
   
+    if (val <= 50 && val >= 25) {
+      
+        return "Yes";
+      
+    }
+    // Only change code above this line
+    return "No";
+  }
+testLogicalAnd(10);
+
+
+function testLogicalOr(val) {
+    // Only change code below this line
+  
+    if (val > 20 || val < 10) {
+      return "Outside";
+    }
+      // Only change code above this line
+    return "Inside";
+  }
+  testLogicalOr(15);
+
+
+  function testElse(val) {
+    let result = "";
+    // Only change code below this line
+  
+    if (val > 5) {
+      result = "Bigger than 5";
+    } else {
+      result = "5 or Smaller";
+    }
+    // Only change code above this line
+    return result;
+  }
+  testElse(4);
+
+
+  function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    } else  if (val < 5) {
+      return "Smaller than 5";
+    } else {
+      return "Between 5 and 10";
+    }
+  }
+  testElseIf(7);
+
+
+  function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  orderMyLogic(7);
+
+
+  function testSize(num) {
+    // Only change code below this line
+  if (num < 5) {
+    return 'Tiny'
+  } else if (num < 10) {
+    return 'Small'
+  } else if (num < 15) {
+    return 'Medium'
+  } else if (num < 20) {
+    return 'Large'
+  } else if (num >= 20) {
+    return 'Huge'
+  } else {
+     return "Change Me";
+  }
+    // Only change code above this line
+  }
+  testSize(7);
+
+
+  const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+  function golfScore(par, strokes) {
+    // Only change code below this line
+  if (strokes == 1){
+    return names[0]
+  } else if (strokes <= par - 2 ){
+    return names[1]
+  } else if (strokes == par - 1 ){
+    return names[2]
+  }else if (strokes == par){
+    return names[3]
+  }else if (strokes == par + 1){
+    return names[4]
+  }else if (strokes == par + 2){
+    return names[5]
+  }else if (strokes >= par + 3){
+    return names[6]
+  } else {
+    return "Change Me";
+  }  
+    // Only change code above this line
+  }
+  golfScore(5, 4);
+  
+  
+  function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+    // Only change code above this line
+    return answer;
+  }
+  caseInSwitch(1);
+
+
+  function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+    case 'a':
+      answer = 'apple';
+      break;
+    case 'b':
+      answer = 'bird';
+      break;
+    case 'c':
+      answer = 'cat';
+      break;
+    default:
+      answer = 'stuff';
+      break;
+  }
+    // Only change code above this line
+    return answer;
+  }
+  switchOfStuff(1);
+
+
+  function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+  switch(val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+  }
+    // Only change code above this line
+    return answer;
+  }
+  sequentialSizes(1);
+
+
+  function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  switch(val){
+  case 'bob':
+    answer="Marley";
+    break;
+  case 42:
+    answer="The Answer";
+    break;
+  case 1:
+    answer="There is no #1";
+    break;
+  case 99:
+    answer="Missed me by this much!";
+    break;
+  case 7:
+    answer="Ate Nine";
+    break;
+  }
+    // Only change code above this line
+    return answer;
+  }
+  chainToSwitch(7);
+  
+  
+  function isLess(a, b) {
+    // Only change code below this line
+       return a < b;
+    
+    // Only change code above this line
+  }
+  isLess(10, 15);
+
+
+  // Setup
+function abTest(a, b) {
+    // Only change code below this line
+  if (a < 0 || b < 0){
+    return undefined;
+  }
+    // Only change code above this line
+   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  abTest(2,2);
+
+
+  let count = 0;
+  function cc(card) {
+    // Only change code below this line
+  switch (card) {
+    case 2: 
+    case 3: 
+    case 4: 
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 7: 
+    case 8:
+    case 9:
+      count = count;
+      break;
+    case 10: 
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+    count -= 1;
+      break;
+  }
+  let result = "";
+  if (count <= 0) {
+    result = " Hold";
+  } else if (count > 0) {
+    result = " Bet"
+  }
+    return count + result;
+    // Only change code above this line
+  }
+  cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+  const myDog = {
+    // Only change code below this line
+  'name': 'artik', 
+  legs: 4,
+  tails: 2,
+  friends: ['wer', 'per', 'perlamutr'],
+    // Only change code above this line
+  };
+
+
+  // Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line
+
+
+ // Setup
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  // Only change code below this line
+  const entreeValue = testObj["an entree"];   // Change this line
+  const drinkValue = testObj["the drink"];    // Change this line
+  
+  
+  // Setup
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  // Only change code below this line
+  const playerNumber = 16;  // Change this line
+  const player = testObj[playerNumber];   // Change this line
+
+  
+
+
+
+
+
+
+
+
 
 
 
