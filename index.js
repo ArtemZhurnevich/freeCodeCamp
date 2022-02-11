23c003962d7721bee9189e708023bf5bf35b4921
ArtemZhurnevich <zhur2305@gmail.com>
2022-02-11 18:43:03 +0300

    completed 85%

--- index.js
+++ index.js
@@ -648,7 +648,159 @@ const testObj = {
   const playerNumber = 16;  // Change this line
   const player = testObj[playerNumber];   // Change this line
 
+
+  const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.bark = "woff";
+
+
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+delete myDog.tails;
+// Only change code below this line
+
+
+// Setup
+function phoneticLookup(val) {
+  // Only change code below this line
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+  };
+  let result = lookup[val];
+  // Only change code above this line
+  return result;
+};
+phoneticLookup("alpha");
+// phoneticLookup("bravo");
+// phoneticLookup("charlie");
+// phoneticLookup("delta");
+// phoneticLookup("echo");
+// phoneticLookup("foxtrot");
+
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
   
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+  // Only change code above this line
+}
+  
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+const secondTree = myPlants[1].list[1];
+
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (prop != 'tracks' && value != '') {
+    records.id.prop = value;
+  } else if (prop === 'tracks' && id.tracks == undefined) {
+    records.id.tracks = ['1999', 'Little Red Corvette']
+  } else if (prop === 'tracks' && value != '') {
+    records.id.tracks.push(value)
+  } else if (value == '') {
+    delete id.prop;
+  }
+  return records;
+}
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
 
 
 
